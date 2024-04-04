7468c5d2658d81d7c8cca6d6b7d1a579cd829a96
petermagar <petermagar4@gmail.com>
2024-04-03 22:02:47 -0400

    Worked on the profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import styles from './App.module.css';
 import { Navbar } from './Components/Navbar/Navbar';
-
+import { Profile} from './Components/Navbar/Profile/Profile';
 
 function App() {
  return <div className={styles.App}>
   
   <Navbar/>
+  <Profile/>
   </div>
 
 };
