a44e2cd357ab11b28e6d6033b3debacb7222f36d
petermagar <petermagar4@gmail.com>
2024-04-16 18:20:41 -0400

    JSON Data created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import styles from './App.module.css';
 import { About } from './Components/About/About';
 import { Education } from './Components/Education/Education';
+import { Experience } from './Components/Experience/Experience';
 import { Navbar } from './Components/Navbar/Navbar';
 import { Profile} from './Components/Profile/Profile';
 
@@ -12,6 +13,7 @@ function App() {
   <Profile/>
   <About />
   <Education/>
+  <Experience/>
   </div>
 
 };
--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,1 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./Experience.module.css";
+import history from "../../data/history.json";
+import { getImageUrl } from "../../utils";
+
+export const Experience = () => {
+  const [selectedExperience, setSelectedExperience] = useState(0);
+
+  useEffect(() => {
+    // Set the selected experience to the index of the first item in the history array
+    setSelectedExperience(0);
+  }, []);
+
+  const handleClick = (index) => {
+    setSelectedExperience(index === selectedExperience ? null : index);
+  };
+
+  return (
+    <div className={styles.experienceContainer}>
+      <section className={styles.container} id="experience">
+        <h2 className={styles.title}>Experience</h2>
+        <div className={styles.layout}>
+          <div className={styles.content}>
+            {/* Main content area */}
+            <div className={styles.main}>
+              {selectedExperience !== null && (
+                <div className={styles.historyItemDetails}>
+                  <h3>
+                    {`${history[selectedExperience].role}, ${history[selectedExperience].organisation}`}
+                  </h3>
+                  <p>{`${history[selectedExperience].startDate} - ${history[selectedExperience].endDate}`}</p>
+                  <ul>
+                    {history[selectedExperience].experiences.map((experience, id) => (
+                      <li key={id}>{experience}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+            
+            {/* Sidebar */}
+            <div className={`${styles.sidebar} ${styles.sidebarRight} ${styles.smallSidebar}`}>
+              <div className={styles.sidebarContainer}>
+                <ul className={styles.history}>
+                  {history.map((historyItem, index) => (
+                    <li
+                      key={index}
+                      className={`${styles.historyItem} ${
+                        index === selectedExperience ? styles.active : ""
+                      }`}
+                      onClick={() => handleClick(index)}
+                    >
+                      <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
