7615a0d09f1f8285b9858862183e1299ed5e3d4a
petermagar <petermagar4@gmail.com>
2024-04-17 14:02:12 -0400

    Fixed fonts

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:layomagarprakash@email.com">layomagarprakash@email.com</a>
+          <a href="mailto:layomagarprakash@email.com">layomagarprakash@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
