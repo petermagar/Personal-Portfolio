cfcc183a0e388dac6ced3146f3e06f0141c6e0df
petermagar <petermagar4@gmail.com>
2024-04-17 12:42:54 -0400

    Project page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Education } from './Components/Education/Education';
 import { Experience } from './Components/Experience/Experience';
 import { Navbar } from './Components/Navbar/Navbar';
 import { Profile} from './Components/Profile/Profile';
+import { Projects } from './Components/Projects/Projects';
+
 
 function App() {
  return <div className={styles.App}>
@@ -14,6 +16,8 @@ function App() {
   <About />
   <Education/>
   <Experience/>
+  <Projects/>
+
   </div>
 
 };
